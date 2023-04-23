1093f8097fc6ec3622a1adfca3401b2f1ca2964e
henry <108975468+Henry-Edet@users.noreply.github.com>
2023-04-23 09:07:22 +0300

    added the collection page and soon the login page

--- js/main.js
+++ js/main.js
@@ -68,13 +68,56 @@ window.addEventListener('click', function (e) {
     }
 })
 
-
+// toggles the close button for acct page of user
 closeButton.addEventListener('click', function() {
     overView.style.display = 'none';
 })
 
-// 
+// toggles the big image to enlarge and shrink back
 const image = document.getElementById("my-image");
 image.addEventListener("click", function() {
     image.classList.toggle("scale");
 });
+
+const image1 = document.getElementById("my-image1");
+image1.addEventListener("click", function() {
+    image1.classList.toggle("scale");
+});
+
+const image2 = document.getElementById("my-image2");
+image2.addEventListener("click", function() {
+    image2.classList.toggle("scale");
+});
+
+const image3 = document.getElementById("my-image3");
+image3.addEventListener("click", function() {
+    image3.classList.toggle("scale");
+});
+
+// slide show for the big photo 
+var slide_index = 1;
+displaySlides(slide_index);
+
+function nextSlide(n) {
+    displaySlides(slide_index += n);
+}
+
+function currentSlide(n) {
+    displaySlides(slide_index = n);
+}
+
+function displaySlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("showSlide");
+    if (n > slides.length) {slide_index = 1}
+    if (n < 1) {slide_index = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slide_index - 1].style.display = "block";
+}
+
+// auto play of images
+window.setInterval(function() {
+    nextSlide(1);
+}, 6000);
